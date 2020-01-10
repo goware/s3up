2445c2e19c8be8cf4f2a9e562a50e05be503775b
Peter Kieltyka <peter.kieltyka@gmail.com>
2020-01-10 15:28:52 -0500

    formatting

--- main.go
+++ main.go
@@ -12,29 +12,23 @@ import (
 var (
 	flags = flag.NewFlagSet("s3up", flag.ExitOnError)
 
-	versionFlag    = flags.Bool("version", false, "print the version and exit")
-	configFlag     = flags.String("config", "", "config file")
-	accessKeyFlag  = flags.String("access-key", "", "s3 access key")
-	secretKeyFlag  = flags.String("secret-key", "", "s3 secret key")
-	regionFlag     = flags.String("region", "", "s3 region")
-	bucketFlag     = flags.String("bucket", "", "s3 bucket")
-	prefixFlag     = flags.String("prefix", "", "s3 path prefix")
-	sourcePathFlag = flags.String("source", "", "local source path to upload")
-	dryrunFlag     = flags.Bool("dryrun", false,
-		"Dryrun, dont upload anything, just list files to upload")
-	confirmFlag = flags.Bool("confirm", false,
-		"Confirm final settings with user before triggering upload")
-	parallelFlag   = flags.Int("parallel", 10, "Number of parallel uploads (default=10)")
-	hashPrefixFlag = flags.Bool("auto-content-hash-prefix", false,
-		"Compute the md5 hash of a file's contents and use it as the path prefix")
-	hashPrefixBytesFlag = flags.Uint("content-hash-bytes", 6,
-		"Number of bytes of md5 hash to use in the path prefix. Max 16. 6 and 12 best (base64 encoding)")
-	syncFlag = flags.Bool("sync", false,
-		"Only upload files that are new or have changed")
-	listFlag = flags.Bool("list", false,
-		"List files data to be processed (outputs in JSON format)")
-	manifestFlag = flags.String("manifest", "", "path to write a manifest file")
-	cacheTTLFlag = flags.Int("cache-ttl", 0, "TTL for cache control headers (in seconds)")
+	versionFlag         = flags.Bool("version", false, "print the version and exit")
+	configFlag          = flags.String("config", "", "config file")
+	accessKeyFlag       = flags.String("access-key", "", "s3 access key")
+	secretKeyFlag       = flags.String("secret-key", "", "s3 secret key")
+	regionFlag          = flags.String("region", "", "s3 region")
+	bucketFlag          = flags.String("bucket", "", "s3 bucket")
+	prefixFlag          = flags.String("prefix", "", "s3 path prefix")
+	sourcePathFlag      = flags.String("source", "", "local source path to upload")
+	dryrunFlag          = flags.Bool("dryrun", false, "Dryrun, dont upload anything, just list files to upload")
+	confirmFlag         = flags.Bool("confirm", false, "Confirm final settings with user before triggering upload")
+	parallelFlag        = flags.Int("parallel", 10, "Number of parallel uploads (default=10)")
+	hashPrefixFlag      = flags.Bool("auto-content-hash-prefix", false, "Compute the md5 hash of a file's contents and use it as the path prefix")
+	hashPrefixBytesFlag = flags.Uint("content-hash-bytes", 6, "Number of bytes of md5 hash to use in the path prefix. Max 16. 6 and 12 best (base64 encoding)")
+	syncFlag            = flags.Bool("sync", false, "Only upload files that are new or have changed")
+	listFlag            = flags.Bool("list", false, "List files data to be processed (outputs in JSON format)")
+	manifestFlag        = flags.String("manifest", "", "path to write a manifest file")
+	cacheTTLFlag        = flags.Int("cache-ttl", 0, "TTL for cache control headers (in seconds)")
 )
 
 const VERSION = "0.3.1"
